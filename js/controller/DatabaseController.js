e127bb5b6d98cfe0bebd65c44c9b2a02e3e98a08
victor <lncwwn@gmail.com>
2015-03-29 23:34:44 +0800

    added feature of show databases

--- js/controller/DatabaseController.js
+++ js/controller/DatabaseController.js
@@ -2,8 +2,6 @@ var databaseModule = angular.module('databaseModule', []);
 
 databaseModule.controller('databaseController', ['$scope', '$routeParams', function($scope, $routeParams) {
 
-	$scope.databases = [{Database: 'dsdsds'}];
-
 	$scope.showDbs = function() {
 		$scope.ipc.send('db-connect', $scope.currentConnection);
 		$scope.setConnectCallback(function() {
@@ -13,29 +11,29 @@ databaseModule.controller('databaseController', ['$scope', '$routeParams', funct
 
 	// set the global current connection
 	if (typeof $routeParams.connectionId !== 'undefined') {
+
+		$scope.databases = [];
+
 		$scope.connections.forEach(function(item, index) {
 			if (item.id == $routeParams.connectionId) {
 				$scope.setCurrentConnection(item);
 			}
 		});
 
-		$scope.databases = [];
-
 		$scope.showDbs();
 
 		$scope.ipc.on('show-dbs-reply', function(arg) {
-			var _database = JSON.parse(JSON.stringify(arg));
+			$scope.$apply(function() {
+				var _database = JSON.parse(JSON.stringify(arg));
 
-			if (_database) {
-				for (var i = 0; i < _database.length; i++) {
-					var _item = _database[i];
-					$scope.databases.push(_item);
+				if (_database) {
+					for (var i = 0; i < _database.length; i++) {
+						var _item = _database[i];
+						$scope.databases.push(_item);
+					}
 				}
-
-				for (var i = 0; i < $scope.databases.length; i++) {
-					console.log($scope.databases[i].Database);
-				}
-			}
+			});
+			
 		});
 
 	}
