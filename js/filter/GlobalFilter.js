46ffc8b480eabdbb44d9d0f0b7d6ba7802732289
victor <lncwwn@gmail.com>
2015-04-05 00:07:06 +0800

    refined code

--- js/filter/GlobalFilter.js
+++ js/filter/GlobalFilter.js
@@ -1,9 +1,11 @@
+/**
+ * the filter module
+ */
 var filterModule = angular.module('filterModule', []);
 
 filterModule.filter('StringLengthFilter', function() {
     return function(input, len) {
-        if (typeof len === 'undefined')
-            len = 12;
+        if (!len) len = 12;
         var result = input || '';
         if (result.length > len) {
             result = result.substr(0, len);
